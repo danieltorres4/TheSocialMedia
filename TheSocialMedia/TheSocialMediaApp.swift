9a62e6cd0ae766055b2f8fd982ad56cec862659f
danieltorres4 <dst990502@outlook.com>
2023-04-03 15:43:44 -0600

    Initial Commit

---
+++ TheSocialMedia/TheSocialMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheSocialMediaApp.swift
+//  TheSocialMedia
+//
+//  Created by Iván Sánchez Torres on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct TheSocialMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
